a2b912a5126ad5408602f8a76360acc61f679793
epiclarkis <jlandoay@jlandoay-Macbook-Pro.local>
2023-09-09 10:33:00 -0400

    Optimized font data encoding logic to avoid RangeError errors

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,8 +3,8 @@ const Input = ({ handleInput }) => {
     return (
         <div className="input">
             <textarea name="input-text" cols="50" rows="5" onChange={handleInput} placeholder="Enter your text here..."></textarea>
-            <p>INSTRUCTIONS: Upload your font file, input some text, then toggle the preview button. This app uses Fira Sans as its default font.</p>
-            <small className="questions">Questions? Ask <a href="https://articulate.slack.com/team/U0126QRG1QD">@johnrey</a></small>
+            <p>Upload your font file, input some text, then toggle the preview button. This app uses Fira Sans as its default font.</p>
+            <p className="questions">Questions? Ask @johnrey</p>
         </div>
     );
 }
